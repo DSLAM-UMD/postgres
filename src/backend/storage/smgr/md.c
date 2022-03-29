4e41832f9a10e71f4d20f91a9f1e576d00e3aa9e
Cuong Nguyen <ctring23@gmail.com>
2022-03-28 22:19:17 -0400

    Assign region directly to relations

--- src/backend/storage/smgr/md.c
+++ src/backend/storage/smgr/md.c
@@ -25,6 +25,7 @@
 #include <fcntl.h>
 #include <sys/file.h>
 
+#include "access/remotexact.h"
 #include "access/xlog.h"
 #include "access/xlogutils.h"
 #include "commands/tablespace.h"
@@ -1067,7 +1068,7 @@ DropRelationFiles(RelFileNode *delrels, int ndelrels, bool isRedo)
 	srels = palloc(sizeof(SMgrRelation) * ndelrels);
 	for (i = 0; i < ndelrels; i++)
 	{
-		SMgrRelation srel = smgropen(delrels[i], InvalidBackendId, 0, 0);
+		SMgrRelation srel = smgropen(delrels[i], InvalidBackendId, 0, UNKNOWN_REGION);
 
 		if (isRedo)
 		{
@@ -1345,7 +1346,7 @@ _mdnblocks(SMgrRelation reln, ForkNumber forknum, MdfdVec *seg)
 int
 mdsyncfiletag(const FileTag *ftag, char *path)
 {
-	SMgrRelation reln = smgropen(ftag->rnode, InvalidBackendId, 0, 0);
+	SMgrRelation reln = smgropen(ftag->rnode, InvalidBackendId, 0, UNKNOWN_REGION);
 	File		file;
 	bool		need_to_close;
 	int			result,

1324359347792cd6c5c1c246c841d70e51b9da73
Cuong Nguyen <ctring23@gmail.com>
2022-04-15 11:37:15 -0400

    Separate hook for GetRegionLsn

--- contrib/zenith/libpagestore.c
+++ contrib/zenith/libpagestore.c
@@ -498,4 +498,6 @@ _PG_init(void)
 	slru_kind_check_hook = zenith_slru_kind_check;
 	slru_read_page_hook = zenith_slru_read_page;
 	slru_page_exists_hook = zenith_slru_page_exists;
+
+	get_region_lsn_hook = get_region_lsn;
 }

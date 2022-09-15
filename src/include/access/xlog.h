2f6a52386d47476d6a5728983fc3562bc3823e1f
Cuong Nguyen <ctring23@gmail.com>
2022-09-14 22:39:38 -0400

    Apply updated and deleted tuples from writeset
    + Port update and delete functions from logical replication.
    + New wal_level remote_xact to generate logical log info without
      wal-logging it.

--- src/include/access/xlog.h
+++ src/include/access/xlog.h
@@ -79,6 +79,7 @@ typedef enum WalLevel
 {
 	WAL_LEVEL_MINIMAL = 0,
 	WAL_LEVEL_REPLICA,
+	WAL_LEVEL_REMOTE_XACT,
 	WAL_LEVEL_LOGICAL
 } WalLevel;
 
@@ -130,6 +131,9 @@ extern PGDLLIMPORT int wal_level;
 
 /* Do we need to WAL-log information required only for logical replication? */
 #define XLogLogicalInfoActive() (wal_level >= WAL_LEVEL_LOGICAL)
+
+/* Do we need to WAL-log information required only for Hot Standby and remote xact? */
+#define XLogRemoteXactInfoActive() (wal_level >= WAL_LEVEL_REMOTE_XACT)
 
 #ifdef WAL_DEBUG
 extern PGDLLIMPORT bool XLOG_DEBUG;

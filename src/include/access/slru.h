aeab8ce2d349e0a2526e6ea159452ba0e4d64d78
Pooja Nilangekar <poojan@umd.edu>
2022-10-16 18:42:15 -0400

    Read slru page at lsn.

--- src/include/access/slru.h
+++ src/include/access/slru.h
@@ -42,9 +42,9 @@
 typedef enum
 {
 	SLRU_PAGE_EMPTY,			/* buffer is not in use */
-	SLRU_PAGE_READ_IN_PROGRESS, /* page is being read in */
+  SLRU_PAGE_READ_IN_PROGRESS, /* page is being read in */
 	SLRU_PAGE_VALID,			/* page is valid and not being written */
-	SLRU_PAGE_WRITE_IN_PROGRESS /* page is being written out */
+  SLRU_PAGE_WRITE_IN_PROGRESS /* page is being written out */
 } SlruPageStatus;
 
 /*
@@ -54,50 +54,51 @@ typedef struct SlruSharedData
 {
 	LWLock	   *ControlLock;
 
-	/* Number of buffers managed by this SLRU structure */
+  /* Number of buffers managed by this SLRU structure */
 	int			num_slots;
 
-	/*
-	 * Arrays holding info for each buffer slot.  Page number is undefined
-	 * when status is EMPTY, as is page_lru_count.
-	 */
+  /*
+   * Arrays holding info for each buffer slot.  Page number is undefined
+   * when status is EMPTY, as is page_lru_count.
+   */
 	char	  **page_buffer;
-	SlruPageStatus *page_status;
+  SlruPageStatus *page_status;
 	bool	   *page_dirty;
 	int		   *page_number;
 	int		   *page_lru_count;
-	LWLockPadded *buffer_locks;
+  XLogRecPtr *page_lsn;
+  LWLockPadded *buffer_locks;
 
-	/*
-	 * Optional array of WAL flush LSNs associated with entries in the SLRU
-	 * pages.  If not zero/NULL, we must flush WAL before writing pages (true
-	 * for pg_xact, false for multixact, pg_subtrans, pg_notify).  group_lsn[]
-	 * has lsn_groups_per_page entries per buffer slot, each containing the
-	 * highest LSN known for a contiguous group of SLRU entries on that slot's
-	 * page.
-	 */
-	XLogRecPtr *group_lsn;
+  /*
+   * Optional array of WAL flush LSNs associated with entries in the SLRU
+   * pages.  If not zero/NULL, we must flush WAL before writing pages (true
+   * for pg_xact, false for multixact, pg_subtrans, pg_notify).  group_lsn[]
+   * has lsn_groups_per_page entries per buffer slot, each containing the
+   * highest LSN known for a contiguous group of SLRU entries on that slot's
+   * page.
+   */
+  XLogRecPtr *group_lsn;
 	int			lsn_groups_per_page;
 
-	/*----------
-	 * We mark a page "most recently used" by setting
-	 *		page_lru_count[slotno] = ++cur_lru_count;
-	 * The oldest page is therefore the one with the highest value of
-	 *		cur_lru_count - page_lru_count[slotno]
-	 * The counts will eventually wrap around, but this calculation still
-	 * works as long as no page's age exceeds INT_MAX counts.
-	 *----------
-	 */
+  /*----------
+   * We mark a page "most recently used" by setting
+   *		page_lru_count[slotno] = ++cur_lru_count;
+   * The oldest page is therefore the one with the highest value of
+   *		cur_lru_count - page_lru_count[slotno]
+   * The counts will eventually wrap around, but this calculation still
+   * works as long as no page's age exceeds INT_MAX counts.
+   *----------
+   */
 	int			cur_lru_count;
 
-	/*
-	 * latest_page_number is the page number of the current end of the log;
-	 * this is not critical data, since we use it only to avoid swapping out
-	 * the latest page.
-	 */
+  /*
+   * latest_page_number is the page number of the current end of the log;
+   * this is not critical data, since we use it only to avoid swapping out
+   * the latest page.
+   */
 	int			latest_page_number;
 
-	/* SLRU's index for statistics purposes (might not be unique) */
+  /* SLRU's index for statistics purposes (might not be unique) */
 	int			slru_stats_idx;
 } SlruSharedData;
 
@@ -111,28 +112,28 @@ typedef struct SlruCtlData
 {
 	SlruShared	shared;
 
-	/*
-	 * Which sync handler function to use when handing sync requests over to
-	 * the checkpointer.  SYNC_HANDLER_NONE to disable fsync (eg pg_notify).
-	 */
-	SyncRequestHandler sync_handler;
+  /*
+   * Which sync handler function to use when handing sync requests over to
+   * the checkpointer.  SYNC_HANDLER_NONE to disable fsync (eg pg_notify).
+   */
+  SyncRequestHandler sync_handler;
 
-	/*
-	 * Decide whether a page is "older" for truncation and as a hint for
-	 * evicting pages in LRU order.  Return true if every entry of the first
-	 * argument is older than every entry of the second argument.  Note that
-	 * !PagePrecedes(a,b) && !PagePrecedes(b,a) need not imply a==b; it also
-	 * arises when some entries are older and some are not.  For SLRUs using
-	 * SimpleLruTruncate(), this must use modular arithmetic.  (For others,
-	 * the behavior of this callback has no functional implications.)  Use
-	 * SlruPagePrecedesUnitTests() in SLRUs meeting its criteria.
-	 */
+  /*
+   * Decide whether a page is "older" for truncation and as a hint for
+   * evicting pages in LRU order.  Return true if every entry of the first
+   * argument is older than every entry of the second argument.  Note that
+   * !PagePrecedes(a,b) && !PagePrecedes(b,a) need not imply a==b; it also
+   * arises when some entries are older and some are not.  For SLRUs using
+   * SimpleLruTruncate(), this must use modular arithmetic.  (For others,
+   * the behavior of this callback has no functional implications.)  Use
+   * SlruPagePrecedesUnitTests() in SLRUs meeting its criteria.
+   */
 	bool		(*PagePrecedes) (int, int);
 
-	/*
-	 * Dir is set during SimpleLruInit and does not change thereafter. Since
-	 * it's always the same, it doesn't need to be in shared memory.
-	 */
+  /*
+   * Dir is set during SimpleLruInit and does not change thereafter. Since
+   * it's always the same, it doesn't need to be in shared memory.
+   */
 	char		Dir[64];
 } SlruCtlData;
 
@@ -140,20 +141,20 @@ typedef SlruCtlData *SlruCtl;
 
 typedef bool (*slru_kind_check_hook_type) (SlruCtl ctl);
 typedef bool (*slru_page_exists_hook_type) (SlruCtl ctl, int segno, off_t offset);
-typedef bool (*slru_read_page_hook_type) (SlruCtl ctl, int segno, off_t offset, char *buffer);
+typedef bool (*slru_read_page_hook_type) (SlruCtl ctl, int segno, off_t offset, XLogRecPtr min_lsn, char *buffer);
 extern PGDLLIMPORT slru_kind_check_hook_type slru_kind_check_hook;
 extern PGDLLIMPORT slru_page_exists_hook_type slru_page_exists_hook;
 extern PGDLLIMPORT slru_read_page_hook_type slru_read_page_hook;
 
 extern Size SimpleLruShmemSize(int nslots, int nlsns);
 extern void SimpleLruInit(SlruCtl ctl, const char *name, int nslots, int nlsns,
-						  LWLock *ctllock, const char *subdir, int tranche_id,
-						  SyncRequestHandler sync_handler);
+                          LWLock *ctllock, const char *subdir, int tranche_id,
+                          SyncRequestHandler sync_handler);
 extern int	SimpleLruZeroPage(SlruCtl ctl, int pageno);
 extern int	SimpleLruReadPage(SlruCtl ctl, int pageno, bool write_ok,
-							  TransactionId xid);
-extern int	SimpleLruReadPage_ReadOnly(SlruCtl ctl, int pageno,
-									   TransactionId xid);
+                             XLogRecPtr min_lsn, TransactionId xid);
+extern int  SimpleLruReadPage_ReadOnly(SlruCtl ctl, int pageno,
+                                       XLogRecPtr min_lsn, TransactionId xid);
 extern void SimpleLruWritePage(SlruCtl ctl, int slotno);
 extern void SimpleLruWriteAll(SlruCtl ctl, bool allow_redirtied);
 #ifdef USE_ASSERT_CHECKING
@@ -165,7 +166,7 @@ extern void SimpleLruTruncate(SlruCtl ctl, int cutoffPage);
 extern bool SimpleLruDoesPhysicalPageExist(SlruCtl ctl, int pageno);
 
 typedef bool (*SlruScanCallback) (SlruCtl ctl, char *filename, int segpage,
-								  void *data);
+                                 void *data);
 extern bool SlruScanDirectory(SlruCtl ctl, SlruScanCallback callback, void *data);
 extern void SlruDeleteSegment(SlruCtl ctl, int segno);
 
@@ -173,8 +174,8 @@ extern int	SlruSyncFileTag(SlruCtl ctl, const FileTag *ftag, char *path);
 
 /* SlruScanDirectory public callbacks */
 extern bool SlruScanDirCbReportPresence(SlruCtl ctl, char *filename,
-										int segpage, void *data);
+                                        int segpage, void *data);
 extern bool SlruScanDirCbDeleteAll(SlruCtl ctl, char *filename, int segpage,
-								   void *data);
+                                   void *data);
 
 #endif							/* SLRU_H */

7ebd25de5205975df3f46779a89a8f5e205ea4e0
Cuong Nguyen <ctring23@gmail.com>
2022-10-18 21:51:50 -0400

    Parameterize csn functions by region & clean up

--- src/backend/storage/ipc/procarray.c
+++ src/backend/storage/ipc/procarray.c
@@ -2249,7 +2249,6 @@ GetSnapshotData(Snapshot snapshot)
 	int			count = 0;
 	int			subcount = 0;
 	bool		suboverflowed = false;
-	XidCSN	xid_csn = FrozenXidCSN;
 	FullTransactionId latest_completed;
 	TransactionId oldestxid;
 	int			mypgxactoff;
@@ -2482,13 +2481,6 @@ GetSnapshotData(Snapshot snapshot)
 	if (!TransactionIdIsValid(MyProc->xmin))
 		MyProc->xmin = TransactionXmin = xmin;
 
-	/*
-	 * Take XidCSN under ProcArrayLock so the snapshot stays
-	 * synchronized.
-	 */
-	if (!snapshot->takenDuringRecovery && get_csnlog_status())
-		xid_csn = GetLastAssignedCSN(false);
-
 	LWLockRelease(ProcArrayLock);
 
 	/* maintain state for GlobalVis* */
@@ -2598,7 +2590,6 @@ GetSnapshotData(Snapshot snapshot)
 	snapshot->copied = false;
 
 	GetSnapshotDataInitOldSnapshot(snapshot);
-	snapshot->snapshot_csn = xid_csn;
 
 	return snapshot;
 }

57104715f0a6a5dff0a8b02a059478efe62bd4e0
Cuong Nguyen <ctring23@gmail.com>
2022-12-15 22:24:11 -0500

    Rename argument

--- src/backend/storage/buffer/bufmgr.c
+++ src/backend/storage/buffer/bufmgr.c
@@ -1604,7 +1604,7 @@ MarkBufferDirty(Buffer buffer)
 
 	if (BufferIsLocal(buffer))
 	{
-		MarkLocalBufferDirty(buffer, false);
+		MarkLocalBufferDirty(buffer, true);
 		return;
 	}
 
@@ -4047,7 +4047,7 @@ MarkBufferDirtyHint(Buffer buffer, bool buffer_std)
 
 	if (BufferIsLocal(buffer))
 	{
-		MarkLocalBufferDirty(buffer, true);
+		MarkLocalBufferDirty(buffer, false);
 		return;
 	}
 

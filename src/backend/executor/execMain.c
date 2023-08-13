694895dc6c569f1c4eb6ddc8bad0c151a957a896
Cuong Nguyen <ctring23@gmail.com>
2023-08-13 14:31:27 -0400

    mechanism to collect time measurement

--- src/backend/executor/execMain.c
+++ src/backend/executor/execMain.c
@@ -65,6 +65,7 @@
 #include "utils/rls.h"
 #include "utils/ruleutils.h"
 #include "utils/snapmgr.h"
+#include "utils/tmeasure.h"
 
 
 /* Hooks for plugins to get control in ExecutorStart/Run/Finish/End */
@@ -484,6 +485,8 @@ standard_ExecutorEnd(QueryDesc *queryDesc)
 	 */
 	Assert(estate->es_finished ||
 		   (estate->es_top_eflags & EXEC_FLAG_EXPLAIN_ONLY));
+
+	log_time_measure();
 
 	/*
 	 * Switch into per-query memory context to run ExecEndPlan
---
+++ src/include/utils/tmeasure.h
@@ -0,0 +1,8 @@
+#ifndef TMEASURE_H
+#define TMEASURE_H
+
+void start_time_measure(const char *name);
+void finish_time_measure(void);
+void log_time_measure(void);
+
+#endif      /* TMEASURE_H */

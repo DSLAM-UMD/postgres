b53d3b81674bc85303f6c211113058038f524bbd
Cuong Nguyen <ctring23@gmail.com>
2023-10-30 14:42:42 -0400

    Not collect relation/page if it is a promotion

--- src/backend/storage/buffer/localbuf.c
+++ src/backend/storage/buffer/localbuf.c
@@ -214,14 +214,14 @@ LocalBufferAlloc(SMgrRelation smgr, ForkNumber forkNum, BlockNumber blockNum,
 
 					*foundPtr = false;
 
-					ereport(DEBUG1, errmsg("Found non-usable buffer %d for (%d, %d, %d). "
+					ereport(DEBUG2, errmsg("Found non-usable buffer %d for (%d, %d, %d). "
 										   "dirty = %d. page_lsn = %X/%X. region_lsn = %X/%X. my_lxid = %d. buffer_lxid = %d.",
 										   b, smgr->smgr_rnode.node.relNode, forkNum, blockNum,
 										   buf_state & BM_DIRTY, LSN_FORMAT_ARGS(page_lsn), LSN_FORMAT_ARGS(region_lsn),
 										   MyProc->lxid, remote_bufHdr->lxid));
 				}
 				else
-					ereport(DEBUG1, errmsg("Found usable buffer %d for (%d, %d, %d). "
+					ereport(DEBUG2, errmsg("Found usable buffer %d for (%d, %d, %d). "
 										   "dirty = %d. page_lsn = %X/%X. region_lsn = %X/%X. my_lxid = %d. buffer_lxid = %d",
 										   b, smgr->smgr_rnode.node.relNode, forkNum, blockNum,
 										   buf_state & BM_DIRTY, LSN_FORMAT_ARGS(page_lsn), LSN_FORMAT_ARGS(region_lsn),
